2a06cc21339892753bed140d9ad3df4ae66df662
Pigeon377 <892197240@qq.com>
2022-05-15 00:12:23 +0800

    feat: implement Query method

--- tree/model/node.go
+++ tree/model/node.go
@@ -1,6 +1,10 @@
 package model
 
 type BPlusTreeNode struct {
+	// the max key in this node
+	// for example
+	// [114, 241, 985, 514]
+	// MaxKey = 514
 	MaxKey int64
 	Nodes  []*BPlusTreeNode
 	Items  []BPlusTreeItem
--- tree/model/tree.go
+++ tree/model/tree.go
@@ -20,12 +20,33 @@ func NewBPlusTree(width int) *BPlusTree {
 	}
 }
 
-func (tree *BPlusTree) Get(key int64) (bool, []byte) {
+func (tree *BPlusTree) Query(key int64) (bool, []byte) {
+	tree.mutex.Lock()
+	defer tree.mutex.Unlock()
+	node := tree.root
+	for i := 0; i < len(node.Nodes); i++ {
+		if key <= node.Nodes[i].MaxKey {
+			node = node.Nodes[i]
+			i = 0
+		}
+	}
 
+	if len(node.Nodes) > 0 {
+		return false, nil
+	}
+
+	for i := 0; i < len(node.Items); i++ {
+		if node.Items[i].Key == key {
+			return true, node.Items[i].Val
+		}
+	}
+	return false, nil
 }
+
 func (tree *BPlusTree) Insert(key int64) (bool, []byte) {
 
 }
+
 func (tree *BPlusTree) Delete(key int64) (bool, []byte) {
 
 }
