ea7b6a80887944d4db9847f8c2c2e21567e9ff00
Pigeon377 <892197240@qq.com>
2022-05-16 21:03:33 +0800

    feat: leaf block get/put/update method

--- disk/leaf_block.go
+++ disk/leaf_block.go
@@ -29,6 +29,9 @@ func NewLeafBlock(
 	}
 }
 
+// ToBytes
+// return
+// convert leaf to bytes slice
 func (leaf *LeafBlock) ToBytes() []byte {
 	if len(leaf.kvs) > blockSize {
 		panic("Too much Key Value Pair")
@@ -50,4 +53,98 @@ func (leaf *LeafBlock) ToBytes() []byte {
 		panic("Node Size Too Large")
 	}
 	return bytes[:4096]
+}
+
+// Put
+// return :
+// false => key exist
+// true  => operation succeed
+func (leaf *LeafBlock) Put(key int64, value []byte) bool {
+	if len(leaf.kvs) == 0 {
+		leaf.kvs = append(leaf.kvs, NewKV(key, value))
+		leaf.kvsSize++
+		leaf.maxKey = key
+		return true
+	}
+	left := int64(0)
+	right := leaf.kvsSize
+	for left < right {
+		mid := (right + left) >> 1
+		if leaf.kvs[mid].Key == key {
+			return false // key exist
+		} else if leaf.kvs[mid].Key > key {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	index := left
+	leaf.kvs = append(leaf.kvs, nil)
+	copy(leaf.kvs[index+1:], leaf.kvs[index:])
+	leaf.kvs[index] = NewKV(key, value)
+	if key > leaf.maxKey {
+		leaf.maxKey = key
+	}
+	leaf.kvsSize++
+	return true
+}
+
+// Get
+// return :
+// true  ,data => query data succeed, return data in second return data
+// false ,nil  => query data failed, key not existed
+func (leaf *LeafBlock) Get(key int64) (bool, []byte) {
+	if len(leaf.kvs) == 0 {
+		return false, nil
+	}
+	if key < leaf.kvs[0].Key || key > leaf.maxKey {
+		return false, nil
+	}
+	left := int64(0)
+	right := leaf.kvsSize
+	for left < right {
+		mid := (right + left) >> 1
+		if leaf.kvs[mid].Key == key {
+			return true, leaf.kvs[mid].Value
+		} else if leaf.kvs[mid].Key > key {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	if leaf.kvs[left].Key == key {
+		return true, leaf.kvs[left].Value
+	}
+	return false, nil
+}
+
+//Update
+//return :
+// false => Update failed, key unexist
+// ture  => Update succeed
+func (leaf *LeafBlock) Update(key int64, value []byte) bool {
+	if len(leaf.kvs) == 0 {
+		return false
+	}
+	if key < leaf.kvs[0].Key || key > leaf.maxKey {
+		return false
+	}
+	left := int64(0)
+	right := leaf.kvsSize
+	for left < right {
+		mid := (right + left) >> 1
+		if leaf.kvs[mid].Key == key {
+			leaf.kvs[mid].Value = value
+			return true
+		} else if leaf.kvs[mid].Key > key {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	if leaf.kvs[left].Key == key {
+		leaf.kvs[left].Value = value
+		return true
+	}
+	return false
 }
