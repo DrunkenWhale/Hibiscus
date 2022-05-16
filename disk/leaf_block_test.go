2c63959ce8f7e7142fffa47386ff81ef27aacd9f
Pigeon377 <892197240@qq.com>
2022-05-16 21:01:26 +0800

    test: leaf operation method

--- disk/leaf_block_test.go
+++ disk/leaf_block_test.go
@@ -2,6 +2,8 @@ package disk
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
@@ -12,4 +14,38 @@ func TestLeafBlock_ToBytes(t *testing.T) {
 		NewKV(114514, []byte("1919810")),
 	})
 	fmt.Println(string(leaf.ToBytes()))
+}
+
+func TestLeafBlock_Put(t *testing.T) {
+	leaf := NewLeafBlock(1, -1, 1, 0, []*KV{})
+	for _, num := range rand.Perm(31) {
+		leaf.Put(int64(num), []byte(strconv.Itoa(num)))
+	}
+	return
+}
+
+func TestLeafBlock_Get(t *testing.T) {
+
+	leaf := NewLeafBlock(1, -1, 1, 0, []*KV{})
+	for _, num := range rand.Perm(31) {
+		leaf.Put(int64(num), []byte(strconv.Itoa(num)))
+	}
+	for _, num := range rand.Perm(31) {
+		ok, ans := leaf.Get(int64(num))
+		fmt.Println(ok, num, "<==>", string(ans))
+	}
+
+}
+
+func TestLeafBlock_Update(t *testing.T) {
+	leaf := NewLeafBlock(1, -1, 1, 0, []*KV{})
+	for _, num := range rand.Perm(31) {
+		leaf.Put(int64(num), []byte(strconv.Itoa(num)))
+	}
+	fmt.Println(leaf.Update(11, []byte("917")))
+	fmt.Println(leaf.Update(211, []byte("917")))
+	for _, num := range rand.Perm(31) {
+		ok, ans := leaf.Get(int64(num))
+		fmt.Println(ok, num, "<==>", string(ans))
+	}
 }
